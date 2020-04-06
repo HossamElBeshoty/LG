abb70c988b3f102e455ab1a697f23dab9e34a249
AmrMossad93 <eng.amrmossad93@gmail.com>
2020-04-07 01:14:42 +0200

    IsoTope

--- src/app/Pages/products-page/safes/safes.component.ts
+++ src/app/Pages/products-page/safes/safes.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+declare function isotopeProduct(): any;
 @Component({
   selector: 'app-safes',
   templateUrl: './safes.component.html',
   styleUrls: ['./safes.component.css']
 })
-export class SafesComponent implements OnInit {
+export class SafesComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  ngAfterViewInit(): void {
+    isotopeProduct();
+  }
 }
