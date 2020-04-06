af1838602c39108c7eed388c7f41262f6f95c38c
AmrMossad93 <eng.amrmossad93@gmail.com>
2020-04-06 16:00:05 +0200

    Product Page

--- src/app/Pages/products-page/products-page.component.ts
+++ src/app/Pages/products-page/products-page.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+declare function owlCarousel(): any;
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
   styleUrls: ['./products-page.component.css']
 })
-export class ProductsPageComponent implements OnInit {
+export class ProductsPageComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  ngAfterViewInit(): void {
+    owlCarousel();
   }
 
 }
