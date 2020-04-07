21b4799199b4293add2d54880ec1a96d01613d68
AmrMossad93 <eng.amrmossad93@gmail.com>
2020-04-07 16:18:10 +0200

    Translation Production Error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomePageComponent } from './Pages/home-page/home-page.component';
-import { NavBarComponent } from './Layouts/nav-bar/nav-bar.component';
-import { FooterComponent } from './Layouts/footer/footer.component';
-import { AboutUsPageComponent } from './Pages/about-us-page/about-us-page.component';
-import { CertificatePageComponent } from './Pages/certificate-page/certificate-page.component';
-import { ProductsPageComponent } from './Pages/products-page/products-page.component';
-import { FaqsPageComponent } from './Pages/faqs-page/faqs-page.component';
-import { ContactUsPageComponent } from './Pages/contact-us-page/contact-us-page.component';
-import { CabinetsComponent } from './Pages/products-page/cabinets/cabinets.component';
-import { VaultsDoorsComponent } from './Pages/products-page/vaults-doors/vaults-doors.component';
-import { MoneyCountingComponent } from './Pages/products-page/money-counting/money-counting.component';
-import { SafesComponent } from './Pages/products-page/safes/safes.component';
-import { CardComponent } from './Widgets/card/card.component';
-import { CardTwoComponent } from './Widgets/card-two/card-two.component';
-import { NgxProgressiveImageLoaderModule, IImageLoaderOptions } from 'ngx-progressive-image-loader';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { NavBarRtlComponent } from './Layouts/nav-bar-rtl/nav-bar-rtl.component';
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomePageComponent} from './Pages/home-page/home-page.component';
+import {NavBarComponent} from './Layouts/nav-bar/nav-bar.component';
+import {FooterComponent} from './Layouts/footer/footer.component';
+import {AboutUsPageComponent} from './Pages/about-us-page/about-us-page.component';
+import {CertificatePageComponent} from './Pages/certificate-page/certificate-page.component';
+import {ProductsPageComponent} from './Pages/products-page/products-page.component';
+import {FaqsPageComponent} from './Pages/faqs-page/faqs-page.component';
+import {ContactUsPageComponent} from './Pages/contact-us-page/contact-us-page.component';
+import {CabinetsComponent} from './Pages/products-page/cabinets/cabinets.component';
+import {VaultsDoorsComponent} from './Pages/products-page/vaults-doors/vaults-doors.component';
+import {MoneyCountingComponent} from './Pages/products-page/money-counting/money-counting.component';
+import {SafesComponent} from './Pages/products-page/safes/safes.component';
+import {CardComponent} from './Widgets/card/card.component';
+import {CardTwoComponent} from './Widgets/card-two/card-two.component';
+import {NgxProgressiveImageLoaderModule, IImageLoaderOptions} from 'ngx-progressive-image-loader';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {NavBarRtlComponent} from './Layouts/nav-bar-rtl/nav-bar-rtl.component';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -51,7 +53,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
+        useFactory: createTranslateLoader,
         deps: [HttpClient]
       }
     }),
@@ -65,7 +67,7 @@ export function HttpLoaderFactory(http: HttpClient) {
       imageRatio: 4 / 3,
       // loading image in placeholder. Can be URL or base64
       placeholderImageSrc:
-        // tslint:disable-next-line:max-line-length
+      // tslint:disable-next-line:max-line-length
         ''
     } as IImageLoaderOptions)
   ],
