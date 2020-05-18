009967c947617ca43210a4e8274b2ff783a9acec
AmrMossad93 <50120275+AmrMossad93@users.noreply.github.com>
2020-05-18 23:09:15 +0200

    About Us Page Us API

--- src/app/Pages/about-us-page/about-us-page.component.ts
+++ src/app/Pages/about-us-page/about-us-page.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {MenuItem} from 'primeng/api';
+import {IPages} from '../../Models/pages';
+import {PageService} from '../../Services/page.service';
+import {ISections} from '../../Models/sections';
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'app-about-us-page',
@@ -10,8 +14,10 @@ export class AboutUsPageComponent implements OnInit {
   language: string = localStorage.getItem('language');
   breadCrumb: MenuItem[];
   homeBreadCrumb: MenuItem;
-
-  constructor() {
+  aboutUsPage: IPages;
+  aboutUsSection: ISections;
+  imgApiPath = environment.imageEndPoint;
+  constructor( public pageService: PageService) {
   }
 
   ngOnInit(): void {
@@ -19,5 +25,16 @@ export class AboutUsPageComponent implements OnInit {
       {label: 'About Us', routerLink: '', styleClass: 'activeBreadCrumb'},
     ];
     this.homeBreadCrumb = {icon: 'pi pi-home', routerLink: '/'};
+    this.getAboutUsPage();
+  }
+
+  getAboutUsPage() {
+    this.pageService.getPageDetails(2).subscribe(res => {
+      this.aboutUsPage = res as IPages;
+    }, error => {
+    }, () => {
+      this.aboutUsSection = this.aboutUsPage.sections.find(c => c.id === 6);
+      console.log(this.aboutUsSection);
+    });
   }
 }
