73d04140c94777be335856ca8be464008e1363a0
AmrMossad93 <50120275+AmrMossad93@users.noreply.github.com>
2020-06-01 23:14:37 +0200

    Removed Un Used Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,22 +2,10 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomePageComponent} from './Pages/home-page/home-page.component';
 import {AboutUsPageComponent} from './Pages/about-us-page/about-us-page.component';
-import {CertificatePageComponent} from './Pages/certificate-page/certificate-page.component';
 import {ProductsPageComponent} from './Pages/products-page/products-page.component';
-import {SafesComponent} from './Pages/products-page/safes/safes.component';
-import {CabinetsComponent} from './Pages/products-page/cabinets/cabinets.component';
-import {VaultsDoorsComponent} from './Pages/products-page/vaults-doors/vaults-doors.component';
-import {MoneyCountingComponent} from './Pages/products-page/money-counting/money-counting.component';
-import {FaqsPageComponent} from './Pages/faqs-page/faqs-page.component';
 import {ContactUsPageComponent} from './Pages/contact-us-page/contact-us-page.component';
-import {ProductsDetailsComponent} from './Components/products-details/products-details.component';
-import {ProductsFilterDetailsComponent} from './Components/products-filter-details/products-filter-details.component';
-import {MoneyCountingDetailsComponent} from './Pages/products-page/money-counting/money-counting-details/money-counting-details.component';
 import {CatalogPageComponent} from './Pages/catalog-page/catalog-page.component';
 import {NotFoundPageComponent} from './Pages/not-found-page/not-found-page.component';
-import {FurniturePageComponent} from './Pages/products-page/furniture-page/furniture-page.component';
-import {LightingComponent} from './Pages/products-page/lighting/lighting.component';
-import {GalleryComponent} from './Pages/products-page/gallery/gallery.component';
 import {ProductCategoryComponent} from "./Components/product-category/product-category.component";
 import {ProductCategoryDetailsComponent} from "./Components/product-category-details/product-category-details.component";
 import {ProductCategoryFilterDetailsComponent} from "./Components/product-category-filter-details/product-category-filter-details.component";
@@ -27,27 +15,12 @@ import {SingleProductComponent} from "./Components/single-product/single-product
 const routes: Routes = [
   {path: '', component: HomePageComponent},
   {path: 'aboutUs', component: AboutUsPageComponent},
-  {path: 'certificate', component: CertificatePageComponent},
   {path: 'products', component: ProductsPageComponent},
-  {path: 'products/safes', component: SafesComponent},
-  {path: 'products/furniture', component: FurniturePageComponent},
-  {path: 'products/lighting', component: LightingComponent},
-  {path: 'products/gallery', component: GalleryComponent},
-  {path: 'products/safes/:name', component: ProductsDetailsComponent},
-  {path: 'products/furniture/:name', component: ProductsDetailsComponent},
-  {path: 'products/lighting/:name', component: ProductsDetailsComponent},
-  {path: 'products/gallery/:name', component: ProductsDetailsComponent},
-  {path: 'products/safes/:name/filter', component: ProductsFilterDetailsComponent},
   {path: 'products/:categoryID', component: ProductCategoryComponent},
   {path: 'products/:productID/filter', component: ProductFilterByTagComponent},
   {path: 'products/:categoryID/:productID', component: ProductCategoryDetailsComponent},
   {path: 'product-details/single-product/:productID', component: SingleProductComponent},
   {path: 'products/:categoryID/:productID/filter', component: ProductCategoryFilterDetailsComponent},
-  {path: 'products/cabinets', component: CabinetsComponent},
-  {path: 'products/vaultsDoors', component: VaultsDoorsComponent},
-  {path: 'products/moneyCounting', component: MoneyCountingComponent},
-  {path: 'products/moneyCounting/:id', component: MoneyCountingDetailsComponent},
-  {path: 'faq', component: FaqsPageComponent},
   {path: 'catalog', component: CatalogPageComponent},
   {path: 'contactUs', component: ContactUsPageComponent},
   {path: 'not-found', component: NotFoundPageComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,8 @@ import {HomePageComponent} from './Pages/home-page/home-page.component';
 import {NavBarComponent} from './Layouts/nav-bar/nav-bar.component';
 import {FooterComponent} from './Layouts/footer/footer.component';
 import {AboutUsPageComponent} from './Pages/about-us-page/about-us-page.component';
-import {CertificatePageComponent} from './Pages/certificate-page/certificate-page.component';
 import {ProductsPageComponent} from './Pages/products-page/products-page.component';
-import {FaqsPageComponent} from './Pages/faqs-page/faqs-page.component';
 import {ContactUsPageComponent} from './Pages/contact-us-page/contact-us-page.component';
-import {CabinetsComponent} from './Pages/products-page/cabinets/cabinets.component';
-import {VaultsDoorsComponent} from './Pages/products-page/vaults-doors/vaults-doors.component';
-import {MoneyCountingComponent} from './Pages/products-page/money-counting/money-counting.component';
-import {SafesComponent} from './Pages/products-page/safes/safes.component';
 import {CardComponent} from './Widgets/card/card.component';
 import {CardTwoComponent} from './Widgets/card-two/card-two.component';
 import {NgxProgressiveImageLoaderModule, IImageLoaderOptions} from 'ngx-progressive-image-loader';
@@ -21,24 +15,17 @@ import {HttpClientModule, HttpClient} from '@angular/common/http';
 import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {CardImageCarouselComponent} from './Widgets/card-image-carousel/card-image-carousel.component';
-import {MoneyCountComponent} from './Components/money-count/money-count.component';
-import {ProductsDetailsComponent} from './Components/products-details/products-details.component';
-import {ProductsFilterDetailsComponent} from './Components/products-filter-details/products-filter-details.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AngularMaterialModule} from './Modules/angular-material.module';
 import {RouteCardComponent} from './Widgets/route-card/route-card.component';
-import {MoneyCountingDetailsComponent} from './Pages/products-page/money-counting/money-counting-details/money-counting-details.component';
 import {PreloaderComponent} from './Components/preloader/preloader.component';
 import {CatalogPageComponent} from './Pages/catalog-page/catalog-page.component';
 import {NotFoundPageComponent} from './Pages/not-found-page/not-found-page.component';
 import {DownloadCardComponent} from './Widgets/download-card/download-card.component';
 import {MoneyCountingCardComponent} from './Widgets/money-counting-card/money-counting-card.component';
-import {FurniturePageComponent} from './Pages/products-page/furniture-page/furniture-page.component';
 import {PrimeNgModule} from './Modules/prime-ng.module';
 import {MathcesCategoryPipe} from './Pipes/filter.pipe';
 import {CategoryCarouselCardComponent} from './Widgets/category-carousel-card/category-carousel-card.component';
-import {LightingComponent} from './Pages/products-page/lighting/lighting.component';
-import {GalleryComponent} from './Pages/products-page/gallery/gallery.component';
 import {SafePipe} from './Pipes/SafePipe';
 import {ProductCategoryComponent} from './Components/product-category/product-category.component';
 import {ProductCategoryDetailsComponent} from './Components/product-category-details/product-category-details.component';
@@ -57,33 +44,20 @@ export function createTranslateLoader(http: HttpClient) {
     NavBarComponent,
     FooterComponent,
     AboutUsPageComponent,
-    CertificatePageComponent,
     ProductsPageComponent,
-    FaqsPageComponent,
     ContactUsPageComponent,
-    CabinetsComponent,
-    VaultsDoorsComponent,
-    MoneyCountingComponent,
-    SafesComponent,
     CardComponent,
     CardTwoComponent,
     CardImageCarouselComponent,
-    MoneyCountComponent,
-    ProductsDetailsComponent,
-    ProductsFilterDetailsComponent,
     RouteCardComponent,
-    MoneyCountingDetailsComponent,
     PreloaderComponent,
     CatalogPageComponent,
     NotFoundPageComponent,
     DownloadCardComponent,
     MoneyCountingCardComponent,
-    FurniturePageComponent,
     MathcesCategoryPipe,
     SafePipe,
     CategoryCarouselCardComponent,
-    LightingComponent,
-    GalleryComponent,
     ProductCategoryComponent,
     ProductCategoryDetailsComponent,
     ProductCategoryFilterDetailsComponent,
